5dab52d6351677a5f68d26874c8ee3dc1d00e9ba
KAIXIN ZHANG <145351389+KevinMonash2022@users.noreply.github.com>
2024-05-27 18:52:56 +1000

    Initial project

---
+++ nft/type/heroicons.d.ts
@@ -0,0 +1,7 @@
+declare module '@heroicons/react/outline' {
+    import { SVGAttributes } from 'react';
+    export interface IconProps extends SVGAttributes<SVGElement> { }
+    export const BellIcon: (props: IconProps) => JSX.Element;
+    export const MenuIcon: (props: IconProps) => JSX.Element;
+    export const XIcon: (props: IconProps) => JSX.Element;
+}
